ca6c05a551e799f63c10d33885d915852db2f5b9
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-05-04 21:42:58 -0300

    - Implementação de Modo Escuro - Ajustes de Componentes - Build para Vercel

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,29 +1,11 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="box">
+  <div class="box formulario">
     <div class="columns">
       <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <section>
-            <strong>
-              {{ tempoDecorrido }}
-            </strong>
-          </section>
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
     </div>
   </div>
@@ -31,32 +13,34 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
-  // eslint-disable-next-line vue/multi-word-component-names
   name: 'Formulario',
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    }
+  emits: ['aoSalvarTarefa'],
+  components: {
+    Temporizador
   },
-  computed: {
-    tempoDecorrido(): string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
+  data(){
+    return {
+      descricao: ''
     }
   },
   methods: {
-    iniciar() {
-      // começar a contagem
-      // 1 seg = 1000 ms 
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1;
-      }, 1000);
-    },
-    finalizar() {
-      clearInterval(this.cronometro);
+    finalizarTarefa(tempoDecorrido: number): void {
+      this.$emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao
+      })
+      this.descricao = '';
     }
   }
-})
+});
 </script>
+
+<style>
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
+</style>
