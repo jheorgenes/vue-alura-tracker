99eb9358577bd10e81aa7e0e04903ff832ec4060
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-05-07 20:21:59 -0300

    [3 Etapa 2 Curso]

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -12,10 +12,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType } from 'vue';
 import Cronometro from './Cronometro.vue';
 import ITarefa from '../interfaces/ITarefa';
-import Box from './Box.vue'
+import Box from './Box.vue';
 
 export default defineComponent({
   name: 'Tarefa',
